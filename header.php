04a72a92381da763fc532495a5b74a94e564545e
Adam Powell <adam@adaminfinitum.com>
2013-11-03 19:42:20 -0500

    modified nav to work with wordpress

--- header.php
+++ header.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" dir="ltr" lnag="en"> <!--<![endif]-->
 <head>
 	<meta charset="utf-8">
 	<title><?php wp_title(''); ?></title>
 	<meta name="viewport" content="width=device-width">
 	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/favicon.ico">
 	<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/apple-touch-icon.png">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700|Noticia+Text:400,700' rel='stylesheet'>
 	<?php wp_head(); ?>
 </head>
 
@@ -15,7 +16,7 @@
 	    <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
 	<![endif]-->
 
-	<div id="container">
+	<div id="container" class="container">
 
 		<header>
 			<a id="logo" href="<?php echo home_url( '/' ); ?>"><?php bloginfo( 'name' ); ?></a>
