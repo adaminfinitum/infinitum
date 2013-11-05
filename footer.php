58f4ab437defc8534c0978e7f59e8efaaaef6d92
Adam Powell <adam@adaminfinitum.com>
2013-11-05 05:54:31 -0500

    styling refinements

--- footer.php
+++ footer.php
@@ -1,9 +1,12 @@
-		<footer class="row clearfix">
-			<?php dynamic_sidebar( 'Footer' ); ?>
-			<p>&copy; <?php echo date( "Y" ); echo " "; bloginfo( 'name' ); ?></p>
-		</footer>
-
 	</div> <!-- /#container -->
+		<footer class="clearfix">
+		    <div class="row clearfix">
+			    <?php dynamic_sidebar( 'Footer' ); ?>
+			</div>
+			<div class="row clearfix">
+			    <p>&copy; <?php echo date( "Y" ); echo " "; bloginfo( 'name' ); ?></p>
+			</div>
+		</footer>
   <script gumby-touch="/wp-content/themes/Wordpress-Starter-Theme/assets/js/gumby/libs"></script>
   <?php wp_footer(); ?>
 
