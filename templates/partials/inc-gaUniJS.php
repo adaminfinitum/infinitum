49f895ede6f8b893282c92d346b98a55a6d09a11
Adam Powell <adam@adaminfinitum.com>
2013-11-12 03:07:51 -0500

    GTM anon

--- templates/partials/inc-gaUniJS.php
+++ templates/partials/inc-gaUniJS.php
@@ -11,7 +11,7 @@
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  gaUni('create', 'UA-XXXXXXX-1', {'siteSpeedSampleRate': 100});
+  gaUni('create', 'UA-XXXXXXX-X', {'siteSpeedSampleRate': 100});
   gaUni('send', 'pageview');
 </script>
 <!-- Rename the global variable if using 2 snippets. Since I was using ga.js first, and have more custom configurations for it,
