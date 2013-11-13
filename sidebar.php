97871cbea40dd4147b7092e67aaba480a8fd78e0
Adam Powell <adam@adaminfinitum.com>
2013-11-13 04:44:01 -0500

    updated sidebar so that colors alternate on social media icons

--- sidebar.php
+++ sidebar.php
@@ -4,17 +4,14 @@
         <script>function googleTranslateElementInit() {new google.translate.TranslateElement({pageLanguage: 'en', gaTrack: true, gaId: 'UA-27155590-1'}, 'google_translate_element');}</script>
         <script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
     </div>
-
     <?php dynamic_sidebar( 'Main Sidebar' ); ?>
-
     <div class="clearfix centered">
-    	<ul class="social-links">
-        <li id="twitter" class="icon-twitter"><a class="animated fadeIn center" href="https://twitter.com/AdamInfinitum"></a></li>
-        <li id="google-plus" class="icon-google-plus"><a class="animated fadeIn center" href="https://google.com/+Adaminfinitum"></a></li>
-        <li id="youtube" class="icon-youtube"><a class="animated fadeIn center" href="https://youtube.com/SemanticWebSEO"></a></li>
-        <li id="rss" class="icon-feed"><a class="animated fadeIn center" href="http://http://feeds.feedburner.com/adaminfinitum"></a></li>
+        <ul class="social-links">
+            <li id="google-plus" class="icon-google-plus"><a class="animated fadeIn center" href="https://google.com/+Adaminfinitum" rel="publisher social"></a></li>
+            <li id="twitter" class="icon-twitter"><a class="animated fadeIn center" href="https://twitter.com/AdamInfinitum" rel="publisher social"></a></li>
+            <li id="youtube" class="icon-youtube"><a class="animated fadeIn center" href="https://youtube.com/SemanticWebSEO" rel="publisher video"></a></li>
+            <li id="rss" class="icon-feed"><a class="animated fadeIn center" href="http://http://feeds.feedburner.com/adaminfinitum" rel="publisher rss feed"></a></li>
         </ul><!-- social-links -->
     </div>
-
 </aside> <!-- /#sidebar -->
 </div>  <!-- /.row -->
